c5f813490665bd45e500dbd3c8b483ca7e84c1b3
Koyo Miyagi <komiyag@amazon.co.jp>
2024-08-13 13:17:49 +0900

    added authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
 
@@ -22,14 +24,23 @@ export default function App() {
       content: window.prompt("Todo content"),
     });
   }
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   return (
+    <Authenticator>
+      {({ signOut, user }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+        {todos.map((todo) => (<li 
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -39,6 +50,9 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
+      )}
+    </Authenticator>
   );
 }

e123a9e731ea6648f7f257363b449d5fff5acf2c
Koyo Miyagi <komiyag@amazon.co.jp>
2024-08-14 15:07:01 +0900

    modify auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,27 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+// defineBackend({
+//  auth,
+//  data,
+// });
+
+const backend = defineBackend({
   auth,
   data,
 });
+
+// override user pool password policies
+backend.auth.resources.cfnResources.cfnUserPool.addPropertyOverride(
+  'Policies',
+  {
+    PasswordPolicy: {
+      MinimumLength: 10,
+      RequireLowercase: true,
+      RequireNumbers: true,
+      RequireSymbols: true,
+      RequireUppercase: true,
+      TemporaryPasswordValidityDays: 20
+    }
+  }
+);

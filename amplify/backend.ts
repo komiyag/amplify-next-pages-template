36fe9720e5a3926b54fde640d0f817ef2acf3722
Koyo Miyagi <komiyag@amazon.co.jp>
2024-08-14 17:21:31 +0900

    modify auth3

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -12,24 +12,44 @@ const backend = defineBackend({
   data,
 });
 
-// override user pool password policies
-backend.auth.resources.cfnResources.cfnUserPool.addPropertyOverride(
-  'Policies',
-  {
-    PasswordPolicy: {
-      MinimumLength: 8,
-      RequireLowercase: true,
-      RequireNumbers: true,
-      RequireSymbols: true,
-      RequireUppercase: true,
-      TemporaryPasswordValidityDays: 20
-    }
-  }
-);
+// extract L1 CfnUserPool resources
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+// modify cfnUserPool policies directly
+cfnUserPool.policies = {
+  passwordPolicy: {
+    minimumLength: 10,
+    requireLowercase: true,
+    requireNumbers: true,
+    requireSymbols: true,
+    requireUppercase: true,
+    temporaryPasswordValidityDays: 20,
+  },
+};
 
-backend.auth.resources.cfnResources.cfnUserPool.addPropertyOverride(
-  'userPoolAddOns',
-  {
-    advancedSecurityMode: 'ENFORCED'
-  }
-);
+cfnUserPool.userPoolAddOns = {
+  advancedSecurityMode: 'ENFORCED',
+};
+
+
+
+// override user pool password policies
+// backend.auth.resources.cfnResources.cfnUserPool.addPropertyOverride(
+//   'Policies',
+//   {
+//     PasswordPolicy: {
+//       MinimumLength: 8,
+//       RequireLowercase: true,
+//       RequireNumbers: true,
+//       RequireSymbols: true,
+//       RequireUppercase: true,
+//       TemporaryPasswordValidityDays: 20
+//     }
+//   }
+// );
+
+// backend.auth.resources.cfnResources.cfnUserPool.addPropertyOverride(
+//   'userPoolAddOns',
+//   {
+//     advancedSecurityMode: 'ENFORCED'
+//   }
+// );
